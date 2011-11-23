762bf20ad779a09eb39a6901115c607ff8743c50
Jesse Mazis <jesmaz1000000@gmail.com>
2011-11-23 18:30:11 +1100

    Block listener should now correctly identify shrines when they are created.

--- src/bukkit/cat/BukkitCatBlockListener.java
+++ src/bukkit/cat/BukkitCatBlockListener.java
@@ -28,6 +28,24 @@ public class BukkitCatBlockListener extends BlockListener {
         
     }
     
+    public int CheckEnds(int[][][] ids, int m){
+        
+        if (ids[0][0][1] == m && ids[2][0][1] == m && ids[0][2][1] == m && ids[2][2][1] == m && ids[1][2][1] == m){
+            
+            if (ids[0][0][0] == 50 && ids[2][0][0] == 50 && ids[0][2][0] == 50 && ids[2][2][0] == 50) return 1;
+            if (ids[0][0][2] == 50 && ids[2][0][2] == 50 && ids[0][2][2] == 50 && ids[2][2][2] == 50) return 2;
+            
+        }
+        if (ids[1][0][0] == m && ids[1][0][2] == m && ids[1][2][0] == m && ids[1][2][2] == m && ids[1][2][1] == m){
+        
+            if (ids[0][0][0] == 50 && ids[0][0][2] == 50 && ids[0][2][0] == 50 && ids[0][2][2] == 50) return 3;
+            if (ids[2][0][0] == 50 && ids[2][0][2] == 50 && ids[2][2][0] == 50 && ids[2][2][2] == 50) return 4;
+        
+        }
+        return 0;
+    
+    }
+    
     public void onBlockPlace(BlockPlaceEvent event){
     
         if (event.getBlock().getTypeId() != 50) return;
@@ -49,7 +67,32 @@ public class BukkitCatBlockListener extends BlockListener {
         
         }
         
-        if (isValidBlockType(bID[1][0][1])==false) return;
+        int m = bID[1][0][1];
+        if (isValidBlockType(m)==false) return;
+        int c = CheckEnds(bID, m);
+        if (c==0) return;
+        
+        if (c<=2) {
+            
+            if (bID[0][1][1] == m && bID[2][1][1] == m){
+                
+                plugin.getServer().broadcastMessage("Shrine created");
+                return;
+                 
+            }
+            
+        }
+        
+        if (c>=3) {
+            
+            if (bID[1][1][0] == m && bID[1][1][2] == m){
+                
+                plugin.getServer().broadcastMessage("Shrine created");
+                return;
+                 
+            }
+            
+        }
         
         
         plugin.getServer().broadcastMessage("torch!");
