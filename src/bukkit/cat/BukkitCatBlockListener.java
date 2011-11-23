ec6528e069371b347a657fb75d8d6335630bfbb3
Jesse Mazis <jesmaz1000000@gmail.com>
2011-11-23 17:39:09 +1100

    Block listener now only responds to torches placed on wooden planks.

--- src/bukkit/cat/BukkitCatBlockListener.java
+++ src/bukkit/cat/BukkitCatBlockListener.java
@@ -21,9 +21,36 @@ public class BukkitCatBlockListener extends BlockListener {
         
     }
     
+    public boolean isValidBlockType (int id){
+        
+        if (id == 5) return true;
+        return false;
+        
+    }
+    
     public void onBlockPlace(BlockPlaceEvent event){
     
         if (event.getBlock().getTypeId() != 50) return;
+        
+        int x = event.getBlock().getX(), y = event.getBlock().getY(), z = event.getBlock().getZ();
+        int bID[][][] = new int[3][3][3];
+        
+        for (int ix=0; ix<3; ix++){
+            
+            for (int iy=0; iy<3; iy++){
+                
+                for (int iz=0; iz<3; iz++){
+                    
+                    bID[ix][iy][iz] = event.getBlock().getWorld().getBlockAt(x + (ix - 1), y + (iy - 1), z + (iz - 1)).getTypeId();
+                    
+                }
+                
+            }
+        
+        }
+        
+        if (isValidBlockType(bID[1][0][1])==false) return;
+        
         
         plugin.getServer().broadcastMessage("torch!");
         return;
