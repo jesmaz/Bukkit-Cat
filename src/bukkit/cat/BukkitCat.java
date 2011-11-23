a6a442a5ce6f316c6d26364a0dec931c13a3a043
Jesse Mazis <jesmaz1000000@gmail.com>
2011-11-23 14:25:34 +1100

    Initial Commit

---
+++ src/bukkit/cat/BukkitCat.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bukkit.cat;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Jesse
+ */
+public class BukkitCat extends JavaPlugin {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    Logger log = Logger.getLogger("Minecraft");
+    
+    public static void main(String[] args) {
+    }
+
+    @Override
+    public void onDisable() {
+        
+        log.info(this + "is disabled");
+        
+    }
+
+    @Override
+    public void onEnable() {
+        
+        log.info(this + "is enabled");
+        
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args)  {
+    
+        return true;
+    
+    }
+    
+}
